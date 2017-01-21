0d9c334a58acb3a86beee4ed85033b1319f4f38b
Llewellyn Thomas <llewellynthomas@Llewellyns-MBP.home>
2017-01-21 14:55:48 +0000

    error with test

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Select2 from 'react-select2-wrapper';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+	
+	React simple starter
+	<Select2 />
+	</div>
     );
   }
 }
